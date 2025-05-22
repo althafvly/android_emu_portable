409ba9f409af6b68bd2ef17887dcabb949416a93
althafvly <althafvly@gmail.com>
2025-05-22 10:27:43 +0530

    Bump to a16

--- start_android_emu.sh
+++ start_android_emu.sh
@@ -9,7 +9,7 @@ export HOME="$PWD/home"
 #=============================
 # Set default values
 #=============================
-BUILD_TOOLS="35.0.1"
+BUILD_TOOLS="36.0.0"
 ANDROID_SDK_ROOT="$PWD/opt/android"
 EMULATOR_NAME="nexus"
 
