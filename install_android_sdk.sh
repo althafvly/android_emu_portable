162ad4e8234a70d2a1ccb696a26564cd935a7cfa
althafvly <althafvly@gmail.com>
2025-05-27 21:30:30 +0530

    use common file for variables

--- install_android_sdk.sh
+++ install_android_sdk.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 set -e
 
-#=============================
-# Set default values
-#=============================
-ARCH="x86_64"
-TARGET="default"
-API_LEVEL="35"
-BUILD_TOOLS="35.0.1"
-ANDROID_API_LEVEL="android-${API_LEVEL}"
-ANDROID_APIS="${TARGET};${ARCH}"
-EMULATOR_PACKAGE="system-images;${ANDROID_API_LEVEL};${ANDROID_APIS}"
-PLATFORM_VERSION="platforms;${ANDROID_API_LEVEL}"
-BUILD_TOOL="build-tools;${BUILD_TOOLS}"
-ANDROID_CMD="commandlinetools-linux-13114758_latest.zip"
-ANDROID_SDK_PACKAGES="${EMULATOR_PACKAGE} ${PLATFORM_VERSION} ${BUILD_TOOL} platform-tools emulator"
-ANDROID_SDK_ROOT="$PWD/opt/android"
+source common.sh
 
 #=============================
 # Install system dependencies
@@ -38,11 +24,7 @@ if [ ! -f "$ANDROID_CMD" ]; then
   mv "$ANDROID_SDK_ROOT/cmdline-tools/"{bin,lib,NOTICE.txt,source.properties} "$ANDROID_SDK_ROOT/cmdline-tools/tools/" || true
 fi
 
-#=============================
-# Set environment variables
-#=============================
-export ANDROID_SDK_ROOT
-export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/tools:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/build-tools/${BUILD_TOOLS}"
+set_env
 
 #=============================
 # Accept licenses and install packages
--- start_android_emu.sh
+++ start_android_emu.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 set -e
 
-#=============================
-# Set home directory
-#=============================
-export HOME="$PWD/home"
-
-#=============================
-# Set default values
-#=============================
-BUILD_TOOLS="35.0.1"
-ANDROID_SDK_ROOT="$PWD/opt/android"
-EMULATOR_NAME="Pixel_6_Pro"
-
-#=============================
-# Set environment variables
-#=============================
-export ANDROID_HOME=$ANDROID_SDK_ROOT
-export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/tools:$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$ANDROID_SDK_ROOT/build-tools/${BUILD_TOOLS}"
+source common.sh
+set_env
 
 #=============================
 # Set headless options if -n is passed
